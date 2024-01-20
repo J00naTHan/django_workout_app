d4410566b865349b501b6999584cdfd284723939
J00naTHan <120300587+J00naTHan@users.noreply.github.com>
2024-01-20 05:32:12 -0300

    Update settings.py

--- exroutine/settings.py
+++ exroutine/settings.py
@@ -85,12 +85,3 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = '/'
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-
-# The absolute path to the directory where collectstatic will collect static files for deployment.
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-
-# The URL to use when referring to static files (where they will be served from)
-STATIC_URL = '/static/'
